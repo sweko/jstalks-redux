48517d647f7774ac92de17136c3a8802b7482836
Wekoslav Stefanovski <swekster@gmail.com>
2018-11-17 00:09:34 +0100

    Small fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'My #Tarabica18 app';
+  title = "My JS Talks '18 app";
 }
